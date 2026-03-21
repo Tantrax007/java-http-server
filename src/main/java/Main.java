b8368f2e702f92e8528ad5f0d3a176a72f278733
Tantrax 007 <nahuelelmejor91@gmail.com>
2026-03-21 16:00:39 +0100

    Feat(Main) Implement use of FileInputStream to use real 8byte chunks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,60 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("File content using 8 bytes chunk: " + readFile());
+//    System.out.println("File content using 8 bytes chunk: " + readFileManually());
+    System.out.println("File content using 8 bytes chunk version 2: " + readFileWithEightBytesBuffer());
   }
 
-  private static String readFile() {
+  private static String readFileWithEightBytesBuffer() {
+    final StringBuilder sb = new StringBuilder();
+
+    // Use of FileInputStream because 'FileReader' is way to limitated
+    try (final FileInputStream fis = new FileInputStream("README.md")) {
+      System.out.println("Total available Bytes: " + fis.available());
+
+      final int buffer = 8;
+      byte[] bytes = new byte[buffer];
+
+      System.out.println("Gonna read from the file 8 bytes at a time");
+
+      while(true){
+        if (fis.available() <= 8){
+          final int lastAvailableBytes = fis.available();
+          final byte[] lastBytes = new byte[lastAvailableBytes];
+          if (fis.read(lastBytes) != lastAvailableBytes) {
+            System.err.println("Couldn't read " + buffer + " bytes");
+            return sb.toString();
+          }
+
+          // Remaining bytes
+          sb.append(new String(lastBytes, 0, lastAvailableBytes, StandardCharsets.UTF_8));
+
+          return sb.toString();
+        }
+
+        if (fis.read(bytes) != buffer) {
+          System.err.println("Couldn't read " + buffer + " bytes");
+          return "";
+        }
+
+        sb.append(new String(bytes, 0, buffer, StandardCharsets.UTF_8));
+      }
+
+    } catch (FileNotFoundException e) {
+      System.err.println("Unable to find file");
+    } catch (IOException io) {
+      System.err.println("Error while opening file");
+    }
+
+    return sb.toString();
+  }
+
+  private static String readFileManually() {
     final StringBuilder sb = new StringBuilder();
     try (final FileReader fis = new FileReader("README.md")) {
       int index = 0;
