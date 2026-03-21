1ad415271d706aadbaab02c777541e8d6c12e970
Tantrax 007 <nahuelelmejor91@gmail.com>
2026-03-21 14:09:16 +0100

    Feat(Main) Reading file 8 bytes at a time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,59 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Contenido del fichero: " + readFile());
+    System.out.println("File content using 8 bytes chunk: " + readFile());
   }
 
-  private static String readFile(){
+  private static String readFile() {
     final StringBuilder sb = new StringBuilder();
-    try (final FileReader fis = new FileReader("README.md")){
-      int intChar;
-      do{
+    try (final FileReader fis = new FileReader("README.md")) {
+      int index = 0;
+      int[] eightChunk = new int[8];
+
+      while (true) {
+        final int intChar; // Local
+
         intChar = fis.read();
-        System.out.println("Caracter leido: " + (char) intChar);
-        sb.append((char) intChar);
-      } while (intChar != -1);
+
+        // If we reach the end
+        if (intChar == -1) return sb.toString();
+
+        eightChunk[index] = intChar;
+
+        // If we have chunk ready
+        if (index == 7) {
+          System.out.println("Chunk ready");
+          index = 0;
+          sb.append(castBuffer(eightChunk));
+        } else {
+          System.out.println("Building chunk");
+          index++;
+        }
+      }
+
     } catch (FileNotFoundException e) {
-      System.err.println("No ha sido posible encontrar el archivo");
+      System.err.println("Unable to find file");
       return "";
-    } catch (IOException io){
-      System.err.println("Error al manipular el fichero README.md");
+    } catch (IOException io) {
+      System.err.println("Error while opening file");
       return "";
     }
+  }
+
+  private static String castBuffer(int[] byteBuffer) {
+    if (byteBuffer.length != 8) {
+      System.err.println("The buffer length is not exactly 8 bytes long");
+      throw new IllegalArgumentException("Invalid byteArray length");
+    }
+
+    final StringBuilder sb = new StringBuilder();
+
+    for (int j : byteBuffer) {
+      System.out.println("Character: " + (char) j);
+      sb.append((char) j);
+    }
+
+    System.out.println("Chunk result: " + sb.toString());
 
     return sb.toString();
   }
