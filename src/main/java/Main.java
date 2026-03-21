61c55029b299f631dcff4f99185a406aaa9db23c
Tantrax 007 <nahuelelmejor91@gmail.com>
2026-03-21 22:48:02 +0100

    Feat(Main) First simple ServerSocket implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,110 +1,49 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import utils.Reader;
+
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-//    System.out.println("File content using 8 bytes chunk: " + readFileManually());
-    System.out.println("File content using 8 bytes chunk version 2: " + readFileWithEightBytesBuffer());
-  }
+    // No need to use InetAddress for the server
+    try (final ServerSocket ssocket = new ServerSocket(42069, 1)) {
+      System.out.println("Server up and running on port: 42069");
+      System.out.println("Awaiting connection...");
 
-  private static String readFileWithEightBytesBuffer() {
-    final StringBuilder sb = new StringBuilder();
+      /**
+       * Blocking call that will wait for a client to initiate
+       * communications and then return with a normal
+       * Socket that is then used for communication with the client.
+       */
+      final Socket connection = ssocket.accept();
 
-    // Use of FileInputStream because 'FileReader' is way to limitated
-    try (final FileInputStream fis = new FileInputStream("README.md")) {
-      System.out.println("Total available Bytes: " + fis.available());
+      // Once we have a connection with a client
+      System.out.println("Connection established in local address: " + connection.getLocalSocketAddress());
+      System.out.println("Client address: " + connection.getRemoteSocketAddress());
 
-      final int buffer = 8;
-      byte[] bytes = new byte[buffer];
+      // Prepare the input stream as we are not going to send anything out
+      final InputStream is = connection.getInputStream();
+      System.out.println("Tamanyo del stream: " + is.available());
 
-      System.out.println("Gonna read from the file 8 bytes at a time");
+      while(true) {
+        // TODO: Improvement, we might want  to add a fix buffer length
+        if (is.available() > 0){
+          final String decodedResponse = Reader.readFileWithEightBytesBuffer(is);
+          System.out.println(decodedResponse);
 
-      while(true){
-        if (fis.available() <= 8){
-          final int lastAvailableBytes = fis.available();
-          final byte[] lastBytes = new byte[lastAvailableBytes];
-          if (fis.read(lastBytes) != lastAvailableBytes) {
-            System.err.println("Couldn't read " + buffer + " bytes");
-            return sb.toString();
-          }
-
-          // Remaining bytes
-          sb.append(new String(lastBytes, 0, lastAvailableBytes, StandardCharsets.UTF_8));
-
-          return sb.toString();
-        }
-
-        if (fis.read(bytes) != buffer) {
-          System.err.println("Couldn't read " + buffer + " bytes");
-          return "";
-        }
-
-        sb.append(new String(bytes, 0, buffer, StandardCharsets.UTF_8));
-      }
-
-    } catch (FileNotFoundException e) {
-      System.err.println("Unable to find file");
-    } catch (IOException io) {
-      System.err.println("Error while opening file");
-    }
-
-    return sb.toString();
-  }
-
-  private static String readFileManually() {
-    final StringBuilder sb = new StringBuilder();
-    try (final FileReader fis = new FileReader("README.md")) {
-      int index = 0;
-      int[] eightChunk = new int[8];
-
-      while (true) {
-        final int intChar; // Local
-
-        intChar = fis.read();
-
-        // If we reach the end
-        if (intChar == -1) return sb.toString();
-
-        eightChunk[index] = intChar;
-
-        // If we have chunk ready
-        if (index == 7) {
-          System.out.println("Chunk ready");
-          index = 0;
-          sb.append(castBuffer(eightChunk));
-        } else {
-          System.out.println("Building chunk");
-          index++;
+          //! Important, because `.read()` is a destructive operation.
+          // If we place this outside the conditional we will end up with
+          // a race condition where `if (is.available() > 0)` is not met and
+          // at the end `is.read()` blocks the thread until the request arrives
+          // to the server, takes the first byte out and loops again which causes
+          // that first byte to be lost.
+          if (is.read() == -1) break;
         }
       }
-
-    } catch (FileNotFoundException e) {
-      System.err.println("Unable to find file");
-      return "";
     } catch (IOException io) {
-      System.err.println("Error while opening file");
-      return "";
+      System.err.println("Something went wrong when trying to listen on port 42069");
     }
-  }
-
-  private static String castBuffer(int[] byteBuffer) {
-    if (byteBuffer.length != 8) {
-      System.err.println("The buffer length is not exactly 8 bytes long");
-      throw new IllegalArgumentException("Invalid byteArray length");
-    }
-
-    final StringBuilder sb = new StringBuilder();
-
-    for (int j : byteBuffer) {
-      System.out.println("Character: " + (char) j);
-      sb.append((char) j);
-    }
-
-    System.out.println("Chunk result: " + sb.toString());
-
-    return sb.toString();
   }
 }
